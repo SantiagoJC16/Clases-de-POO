aab6c106f4842174867c405060173135e0c9f66f
SantiagoJC16 <108434287+SantiagoJC16@users.noreply.github.com>
2025-06-30 12:57:44 -0500

    Add files via upload

---
+++ Componentes_y_Objetos_graficos2/src/main/java/com/mycompany/componentes_y_objetos_graficos2/Componentes_y_Objetos_graficos2.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.componentes_y_objetos_graficos2;
+
+
+public class Componentes_y_Objetos_graficos2 {
+
+    
+}
---
+++ Componentes_y_Objetos_graficos2/src/main/java/com/mycompany/componentes_y_objetos_graficos2/Formulario.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.componentes_y_objetos_graficos2;
+
+public class Formulario{
+    
+    public static void main(String args[]) {
+        
+        Form formulario = new Form();
+        formulario.setVisible(true);
+        
+    }
+}
